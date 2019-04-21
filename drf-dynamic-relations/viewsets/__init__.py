88baa7303e570554f396d270ae1909e7b6e7223a
FLATCOKE <flatcoke89@gmail.com>
2019-04-21 22:26:01 +0900

    add include to viewsets

--- drf-dynamic-relations/viewsets/__init__.py
+++ drf-dynamic-relations/viewsets/__init__.py
@@ -2,6 +2,7 @@
 For to change the lazy load to eager load while using the dynamic field
 in viewsets of django restframework
 """
+from rest_framework.serializers import FieldDoesNotExist
 
 
 class DynamicRelationsMixin(object):
@@ -14,19 +15,27 @@ class DynamicRelationsMixin(object):
         Filters the fields according to the `fields` in query parameter.
         it will be improved performance using `prefetch_related`
         """
-        query_in_fields = self.request.query_params.get('fields')
-        if query_in_fields is not None:
-            quern_fields = set(query_in_fields.split(','))
+        query_in_fields = []
+        for i in self.QUERY_KEYS:
+            try:
+                query_in_fields = query_in_fields + \
+                                  self.request.query_params.get(i).split(',')
+            except AttributeError:
+                pass
 
+        if query_in_fields:
             model_meta = self.queryset.model._meta
             relationables = []
-            for field in quern_fields:
-                model_field = model_meta.get_field(field)
-                if type(model_field).__name__ in ('ForeignKey',
-                                                  'ManyToManyRel',
-                                                  'ManyToOneRel'):
-                    relationables.append(field)
+            for field in query_in_fields:
+                try:
+                    model_field = model_meta.get_field(field)
+                    if type(model_field).__name__ in ('ForeignKey',
+                                                      'ManyToManyRel',
+                                                      'ManyToOneRel'):
+                        relationables.append(field)
+                except FieldDoesNotExist:
+                    pass
             if relationables:
                 self.queryset = self.queryset.prefetch_related(*relationables)
 
-        return super(DynamicRelationsMixin, self).get_queryset()
+        return super(DynamicRelationMixin, self).get_queryset()
